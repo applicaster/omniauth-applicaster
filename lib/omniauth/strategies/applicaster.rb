3c3a903e1d4b7a6ba6c316c73ba9128c745a1aca
Neer Friedman <neerfri@gmail.com>
2016-10-27 12:12:12 +0300

    Fix issue with redirect_uri not identical between code and token phases
    See:
    https://github.com/intridea/omniauth-oauth2/issues/93
    https://github.com/intridea/omniauth-oauth2/issues/93#issuecomment-227247801
    https://github.com/intridea/oauth2/pull/269

--- lib/omniauth/strategies/applicaster.rb
+++ lib/omniauth/strategies/applicaster.rb
@@ -26,6 +26,10 @@ module OmniAuth
       def client
         ::Applicaster::Accounts.oauth_client
       end
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
