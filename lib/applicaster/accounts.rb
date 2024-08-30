7f113250d2f308bbc8a1b1e93e95d93c32139e48
yuliroth <y.roth@applicaster.com>
2024-08-30 18:26:34 +0300

    feat(logs): adds more detailed logs

--- lib/applicaster/accounts.rb
+++ lib/applicaster/accounts.rb
@@ -20,7 +20,7 @@ module Applicaster
 
         Faraday.new(conn_opts) do |conn|
           if options[:token]
-            conn.request :oauth2, options[:token], token_type: 'param'
+            conn.request :oauth2, options[:token], token_type: "param"
           end
 
           conn.request :json
@@ -38,8 +38,6 @@ module Applicaster
 
 
           conn.response :json, content_type: /\bjson$/
-          # conn.response :logger, Rails.logger
-          # conn.response :logger, Logger.new(STDOUT)
           conn.response :raise_error
 
           conn.adapter config.faraday_adapter
@@ -47,15 +45,22 @@ module Applicaster
       end
 
       def user_from_token(token)
-        Applicaster::Accounts::User.new(
+        Rails.logger.info("Fetching user with token: #{token}")
+        user = Applicaster::Accounts::User.new(
           connection(token: token)
             .get("/api/v1/users/current.json")
             .body
         )
+        if user.nil?
+          Rails.logger.error("[Login Failed] -  User fetch failed. Token: #{token}, IP: #{request.remote_ip}, User Agent: #{request.user_agent}")
+        end
+        user
       rescue Faraday::ClientError => e
         if e.response && e.response[:status] == 401
+          Rails.logger.error("[Login Failed] - Unauthorized access attempt detected. Invalid token: #{token}, IP: #{request.remote_ip}, User Agent: #{request.user_agent}, Error: #{e.message}")
           nil
         else
+          Rails.logger.error("[Login Failed] - Error fetching user. Token: #{token}, IP: #{request.remote_ip}, User Agent: #{request.user_agent}, Error: #{e.message}")
           raise
         end
       end
@@ -66,13 +71,20 @@ module Applicaster
             .get("/api/v1/users/#{id}.json")
             .body
         )
+      rescue Faraday::ResourceNotFound
+        Rails.logger.error("[Login Failed] - User not found. ID: #{id}, Token: #{token}, IP: #{request.remote_ip}, User Agent: #{request.user_agent}")
+        nil
       end
 
       def accounts_from_token(token)
+        Rails.logger.info("Fetching accounts with token: #{token}")
         connection(token: token)
           .get("/api/v1/accounts.json")
           .body
           .map {|a| Account.new(a) }
+      rescue Faraday::ClientError => e
+        Rails.logger.error("Failed to fetch accounts. Token: #{token}, Error: #{e.message}")
+        raise
       end
 
       def config
@@ -90,12 +102,15 @@ module Applicaster
           site: config.base_url,
           authorize_url: "/oauth/authorize",
           auth_scheme: :basic_auth,
-        )
+          )
       end
     end
 
     def user_data_from_omniauth(omniauth_credentials)
       access_token(omniauth_credentials).get("/api/v1/users/current.json").parsed
+    rescue Faraday::ClientError => e
+      Rails.logger.error("[Login Failed] - Failed to fetch user data from Omniauth. Error: #{e.message}")
+      raise
     end
 
     def accounts
@@ -103,8 +118,10 @@ module Applicaster
     end
 
     def find_user_by_id(id)
+      Rails.logger.info("Finding user by ID: #{id}")
       self.class.user_by_id_and_token(id, client_credentials_token.token)
     rescue Faraday::ResourceNotFound
+      Rails.logger.error("[Login Failed] - User not found by ID: #{id}")
       nil
     end
 
@@ -118,6 +135,9 @@ module Applicaster
       @client_credentials_token ||= self.class.oauth_client
         .client_credentials
         .get_token
+    rescue OAuth2::Error => e
+      Rails.logger.error("[Login Failed] - Failed to get client credentials token. Error: #{e.message}")
+      raise
     end
   end
 end
--- lib/applicaster/sessions_controller_mixin.rb
+++ lib/applicaster/sessions_controller_mixin.rb
@@ -1,25 +1,32 @@
 module Applicaster
   module SessionsControllerMixin
     def new
+      Rails.logger.info("New session initiated. Redirecting to /auth/applicaster. IP: #{request.remote_ip}, User Agent: #{request.user_agent}")
       redirect_to "/auth/applicaster"
     end
 
     def create
       session[:omniauth_credentials] = omniauth_credentials
+      Rails.logger.info("Session created successfully for user. IP: #{request.remote_ip}, User Agent: #{request.user_agent}, Params: #{safe_user_params.inspect}")
 
       redirect_to(session.delete(:path_before_login) || '/')
     end
 
     def destroy
-      reset_session
+      user_email = current_user.email rescue "Unknown"
+      Rails.logger.info("Session destroyed for user: #{user_email}. IP: #{request.remote_ip}, User Agent: #{request.user_agent}")
 
-      redirect_to "/"
+      reset_session
+      redirect_to config.base_url
     end
 
     def failure
-      Rails.logger.warn({
-        message: "Omniauth error with strategy '#{params[:strategy]}': #{params[:message]}",
+      Rails.logger.error({
+        message: "[Login Failed] - Omniauth error with strategy '#{params[:strategy]}': #{params[:message]}",
         origin: params[:origin],
+        IP: request.remote_ip,
+        UserAgent: request.user_agent,
+        Params: params[:origin],
       })
       flash[:notice] = "There was a problem logging in"
       redirect_to "/"
